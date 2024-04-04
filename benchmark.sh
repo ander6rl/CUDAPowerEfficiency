86688be388de358f75af47728015ab548b4e6fd4
Sarah Pinkstaff <pinkstse1@gmail.com>
2024-04-04 18:27:30 -0400

    Changed script to work with mixbench-cuda.

--- benchmark.sh
+++ benchmark.sh
@@ -7,9 +7,10 @@ delay=$(echo "scale=3; $sampling_rate_ms / 1000" | bc)
 log_file="gpu_sensor_log.txt"
 
 echo "Timestamp (s), Power (W), Performance State" > "$log_file"
-
+max_block_size=$((1024*1024*64))
+block=1
 while true; do
-
+    echo "srun --gres=gpu ./mixbench-cuda -b $block"
     gpu_info=$(nvidia-smi --query-gpu=timestamp,power.draw,pstate --format=csv,noheader,nounits)
 
 
@@ -18,5 +19,9 @@ while true; do
     pstate=$(echo "$gpu_info" | cut -d ',' -f 3)
 
     echo "$timestamp, $power, $pstate" >> "$log_file"
+    block=$(($block*2))
+    if [[$block > $max_block_size]]
+    then
+        break
     sleep $delay
 done
