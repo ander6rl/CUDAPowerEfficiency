80ef912a4c7af25c1363d3cd4b7c160dfd85716a
Rebecca Anderson <ander6rl@gmail.com>
2024-04-05 16:59:44 -0400

    Made modifications to benchmark mixbench to potentially allow for runtime changes of CUDA blocksize [THIS WILL NOT WORK BECAUSE CUDA DOES NOT ALLOW FOR RUNTIME CHANGES TO BLOCKSIZE, ONLY CAN BE SET IN COMPILE TIME]

---
+++ modified_mixbench-cuda/mixbench-cuda/main-cuda.cpp
@@ -0,0 +1,52 @@
+/**
+ * main-cuda.cpp: This file is part of the mixbench GPU micro-benchmark suite.
+ *
+ * Contact: Elias Konstantinidis <ekondis@gmail.com>
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <string.h>
+#include "lcutil.h"
+#include "mix_kernels_cuda.h"
+#include "version_info.h"
+
+#define VECTOR_SIZE (32 * 1024 * 1024)
+
+int main(int argc, char* argv[]) {
+    printf("mixbench (%s)\n", VERSION_INFO);
+
+    int blockSize = 256; // default block size
+    bool runWarmup = false;
+
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-b") == 0 && i + 1 < argc) {
+            blockSize = atoi(argv[i + 1]);
+        } else if (strcmp(argv[i], "-w") == 0) {
+            runWarmup = true;
+        }
+    }
+
+    unsigned int datasize = VECTOR_SIZE * sizeof(double);
+
+    cudaSetDevice(0);
+    StoreDeviceInfo(stdout);
+
+    size_t freeCUDAMem, totalCUDAMem;
+    cudaMemGetInfo(&freeCUDAMem, &totalCUDAMem);
+    printf("Total GPU memory %lu, free %lu\n", totalCUDAMem, freeCUDAMem);
+    printf("Buffer size:          %dMB\n", datasize / (1024 * 1024));
+    printf("Block size:           %d\n", blockSize);
+    printf("Running warmup:       %s\n", runWarmup ? "yes" : "no");
+
+    double* c;
+    c = (double*)malloc(datasize);
+
+    mixbenchGPU(c, VECTOR_SIZE, blockSize, runWarmup);
+
+    free(c);
+
+    return 0;
+}
---
+++ modified_mixbench-cuda/mixbench-cuda/mix_kernels_cuda.h
@@ -0,0 +1,9 @@
+/**
+ * mix_kernels_cuda.h: This file is part of the mixbench GPU micro-benchmark suite.
+ *
+ * Contact: Elias Konstantinidis <ekondis@gmail.com>
+ **/
+
+#pragma once
+
+extern "C" void mixbenchGPU(double*, long size, int b, bool runWarmup);
