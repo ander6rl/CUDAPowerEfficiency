d0e67ee06769922a2f9ffda503dda317077bc0c0
Rebecca Anderson <ander6rl@gmail.com>
2024-04-10 10:10:13 -0400

    Added built_block files for running benchmarks

---
+++ build_block_1/CMakeFiles/mixbench-cuda.dir/main-cuda.cpp.o.d
@@ -0,0 +1,70 @@
+CMakeFiles/mixbench-cuda.dir/main-cuda.cpp.o: \
+ /nfs/home/ander6rl/cs470/Research/CUDAPowerEfficiency/mixbench/mixbench-cuda/main-cuda.cpp \
+ /usr/include/stdc-predef.h /usr/include/stdio.h \
+ /usr/include/bits/libc-header-start.h /usr/include/features.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/stddef.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/stdarg.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/c++/8/stdlib.h /usr/include/c++/8/cstdlib \
+ /usr/include/c++/8/x86_64-redhat-linux/bits/c++config.h \
+ /usr/include/c++/8/x86_64-redhat-linux/bits/os_defines.h \
+ /usr/include/c++/8/x86_64-redhat-linux/bits/cpu_defines.h \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h /usr/include/bits/types/locale_t.h \
+ /usr/include/bits/types/__locale_t.h /usr/include/sys/types.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/time_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h /usr/include/c++/8/bits/std_abs.h \
+ /usr/local/cuda/targets/x86_64-linux/include/cuda.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-uintn.h \
+ /usr/local/cuda/targets/x86_64-linux/include/cuda_runtime.h \
+ /usr/local/cuda/targets/x86_64-linux/include/crt/host_config.h \
+ /usr/local/cuda/targets/x86_64-linux/include/builtin_types.h \
+ /usr/local/cuda/targets/x86_64-linux/include/device_types.h \
+ /usr/local/cuda/targets/x86_64-linux/include/crt/host_defines.h \
+ /usr/local/cuda/targets/x86_64-linux/include/driver_types.h \
+ /usr/local/cuda/targets/x86_64-linux/include/vector_types.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/limits.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /usr/include/bits/uio_lim.h \
+ /usr/local/cuda/targets/x86_64-linux/include/surface_types.h \
+ /usr/local/cuda/targets/x86_64-linux/include/texture_types.h \
+ /usr/local/cuda/targets/x86_64-linux/include/library_types.h \
+ /usr/local/cuda/targets/x86_64-linux/include/channel_descriptor.h \
+ /usr/local/cuda/targets/x86_64-linux/include/cuda_runtime_api.h \
+ /usr/local/cuda/targets/x86_64-linux/include/cuda_device_runtime_api.h \
+ /usr/local/cuda/targets/x86_64-linux/include/driver_functions.h \
+ /usr/local/cuda/targets/x86_64-linux/include/vector_functions.h \
+ /usr/local/cuda/targets/x86_64-linux/include/vector_functions.hpp \
+ /usr/include/c++/8/utility /usr/include/c++/8/bits/stl_relops.h \
+ /usr/include/c++/8/bits/stl_pair.h /usr/include/c++/8/bits/move.h \
+ /usr/include/c++/8/bits/concept_check.h /usr/include/c++/8/type_traits \
+ /usr/include/c++/8/initializer_list /usr/include/string.h \
+ /usr/include/strings.h \
+ /nfs/home/ander6rl/cs470/Research/CUDAPowerEfficiency/mixbench/mixbench-cuda/lcutil.h \
+ /usr/local/cuda/targets/x86_64-linux/include/cuda_runtime_api.h \
+ /nfs/home/ander6rl/cs470/Research/CUDAPowerEfficiency/mixbench/mixbench-cuda/mix_kernels_cuda.h \
+ /nfs/home/ander6rl/cs470/Research/CUDAPowerEfficiency/mixbench/mixbench-cuda/version_info.h
---
+++ build_block_1024/CMakeFiles/mixbench-cuda.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/nfs/home/ander6rl/cs470/Research/CUDAPowerEfficiency/mixbench/mixbench-cuda/mix_kernels_cuda.cu" "CMakeFiles/mixbench-cuda.dir/mix_kernels_cuda.cu.o" "gcc" "CMakeFiles/mixbench-cuda.dir/mix_kernels_cuda.cu.o.d"
+  "/nfs/home/ander6rl/cs470/Research/CUDAPowerEfficiency/mixbench/mixbench-cuda/main-cuda.cpp" "CMakeFiles/mixbench-cuda.dir/main-cuda.cpp.o" "gcc" "CMakeFiles/mixbench-cuda.dir/main-cuda.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
