0a12e7b8d12c658a69738655adc3345e7d8d3826
Rebecca Anderson <ander6rl@gmail.com>
2024-04-11 13:11:29 -0400

    changed powerreader to start benchmark one second after start and ends one second after benchmarks ends

--- powerreader.sh
+++ powerreader.sh
@@ -4,37 +4,25 @@
 
 # This shell scipt just reads the power level in watts fron nvidia-smi and writes it to a file.
 
-# It will only terminate when the terminal that ran it types power-done
-
-
-
-
-
+# 10 milliseconds sampling rate
 sampling_rate_ms=10
 
-
-
 delay=$(echo "scale=3; $sampling_rate_ms / 1000" | bc)
-
-
 
 log_file="gpu_sensor_log.txt"
 
 
+# Execute benchmark in the background, wait 2 seconds, then start and redirect output to results.txt
+(sleep 1 && echo "--------------------Benchmark starting--------------------" && build/mixbench-cuda -b $1> results.txt && echo "--------------------Benchmark done!--------------------" && sleep 1 && kill $$) &
 
 
-
-(sleep 2 && echo "--------------------Benchmark starting--------------------" && build/mixbench-cuda -b $1> results.txt && echo "--------------------Benchmark done!--------------------") &
-
-
-
+echo "Timestamp (s), Power (W), Performance State"
 echo "Timestamp (s), Power (W), Performance State" > "$log_file"
 
+# Infinite loop to continously monitor GPU
 while true; do
 
     gpu_info=$(nvidia-smi --query-gpu=timestamp,power.draw,pstate --format=csv,noheader,nounits)
-
-
 
     timestamp=$(echo "$gpu_info" | cut -d ',' -f 1)
 
@@ -42,16 +30,11 @@ while true; do
 
     pstate=$(echo "$gpu_info" | cut -d ',' -f 3)
 
-
-
     echo "$timestamp, $power, $pstate"
 
     echo "$timestamp, $power, $pstate" >> "$log_file"
 
-
-
     sleep $delay
-
 
 
 done
